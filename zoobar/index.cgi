f0739c6d9c85629fe6274285ce3e01cd144c3425
Jose Reyes <josreyes53@gmail.com>
2018-01-09 01:25:36 -0400

    clearing up for lab2

--- zoobar/index.cgi
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from wsgiref.handlers import CGIHandler
-
-from __init__ import *
-
-if __name__ == "__main__":
-    CGIHandler().run(app)
--- zoobar/templates/zoobars.js
+++
@@ -1,7 +0,0 @@
-
-var myZoobars = {{ g.user.zoobars if g.user.zoobars > 0 else 0 }};
-
-var div = document.getElementById("myZoobars");
-if (div != null) {
-    div.innerHTML = myZoobars;
-}
